67d9e8fce4994698af5413e06e5b60c2db3cafe6
Bruce Beare <bruce.j.beare@intel.com>
2016-04-07 11:58:48 -0700

    chaabi: initial commit for tests
    Pulled in from the fugu tree and cleaned up
    
    Tracked-On: https://jira01.devtools.intel.com/browse/BP-21
    Change-Id: I060c78a9b5d4c1c57898affb2212c42560684f71
    Signed-off-by: Khushboo Bindlish <khushboo.bindlish@intel.com>
    Signed-off-by: Bruce Beare <bruce.j.beare@intel.com>

---
+++ peripheral/keystore/chaabi/tests/key_store/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE := intel_keymaster_test
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := intel_keymaster_test.cpp
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../src
+
+LOCAL_CFLAGS = -Wall -Werror
+LOCAL_SHARED_LIBRARIES := libcutils libkeymaster
+
+include $(BUILD_EXECUTABLE)
